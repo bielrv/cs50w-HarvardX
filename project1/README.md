da60f6bcba358a70ba6fab295c15f37207fba806
Bielrv <rosinol.gabriel@gmail.com>
2018-08-05 16:57:51 +0200

    First steps project 1

--- project1/README.md
+++ project1/README.md
@@ -1,3 +1,11 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+## Setup  
+`export FLASK_APP=application.py`    
+`export DATABASE_URL=postgres://ficpljxencvllu:f3ece79580b1452c6f01aaccbe5120703ac6bf59652b3871ef2b7f76ed443d1e@ec2-54-247-79-32.eu-west-1.compute.amazonaws.com:5432/dftm1idkv20mvc`  
+`pip install jupyter`  
+`ipython kernel install --user --name "cs50_Porject1"`
+
+`export FLASK_DEBUG=1`
--- project1/application.py
+++ project1/application.py
@@ -1,9 +1,14 @@
 import os
-
-from flask import Flask, session
+import requests
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+os.getcwd()
+
+# res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "0yOQWbyRCLpwoPzvLkCe9Q", "isbns": "9781632168146"})
+# print(res.json())
 
 app = Flask(__name__)
 
@@ -20,7 +25,11 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/hello",methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html",name=name)
